338a9c4a86bdd411a532aa00cdf769cc0dd67e66
Callum Flanagan <Callumaflanagan@gmail.com>
2025-01-03 17:59:57 +0000

    basic get random line code

---
+++ module1.py
@@ -0,0 +1,26 @@
+
+
+# Script to make tutorial image
+
+import linecache 
+import  random
+
+infoFileName = "Information.txt"  
+
+# on script start 
+# open file and get a random line
+# open the splash image template
+# print the selected line on an image using the right format
+# save the image to the location of the splash image
+
+# Get number of lines
+with open(infoFileName, 'r') as fp:
+    for numOfLines, line in enumerate(fp):
+        pass
+
+    
+print(numOfLines+1)
+SelectedLine = random.randint(0, numOfLines+1)
+
+# Print a random Line
+print(linecache.getline(infoFileName, SelectedLine))
