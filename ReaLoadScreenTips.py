67fb42523609c2c3d6e367cedd9abae78935c9b6
Callum Flanagan <Callumaflanagan@gmail.com>
2025-01-03 18:42:14 +0000

    implemented basic image manipulation

--- ReaLoadScreenTips.py
+++ ReaLoadScreenTips.py
@@ -1,12 +1,15 @@
 
-
 # Script to make tutorial image
-
 import linecache 
 import random
 
-infoFileName = "Information.txt"  
+# Image Libraries
+from PIL import Image
+from PIL import ImageDraw
 
+infoFileName = "Information.txt"  
+splashTemplate = "Reaper Splash.png"
+editedSplashName = "Splash.png"
 # on script start 
 # open file and get a random line
 # open the splash image template
@@ -23,4 +26,22 @@ print(numOfLines+1)
 SelectedLine = random.randint(0, numOfLines+1)
 
 # Print a random Line
-print(linecache.getline(infoFileName, SelectedLine))
+Tip = linecache.getline(infoFileName, SelectedLine)
+print(Tip)
+
+### Image Gen
+
+# Open Splash Template
+img = Image.open(splashTemplate)
+ 
+# Call draw Method to add 2D graphics in an image
+I1 = ImageDraw.Draw(img)
+ 
+# Add Text to an image
+I1.text((28, 36), Tip, fill=(255, 0, 0))
+ 
+# Display edited image
+img.show()
+ 
+# Save the edited image
+img.save(editedSplashName)
